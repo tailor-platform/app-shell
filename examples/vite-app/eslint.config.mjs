954ab5d9799cbb9d7bdfc41a2546ebec7e119071
IzumiSy <beetle-noise@gmx.com>
2026-02-27 15:16:51 +0900

    Fix deleted import that is used

--- examples/vite-app/eslint.config.mjs
+++ examples/vite-app/eslint.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";

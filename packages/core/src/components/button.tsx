d0868f3a5f0ab8bc172ad6754e7596334b1b1640
IzumiSy <beetle-noise@gmx.com>
2026-03-13 23:03:23 +0900

    Export button component

--- packages/core/src/components/button.tsx
+++ packages/core/src/components/button.tsx
@@ -35,17 +35,12 @@ const buttonVariants = cva(
   },
 );
 
-function Button({
-  className,
-  variant,
-  size,
-  render,
-  children,
-  ...props
-}: React.ComponentProps<"button"> &
+type ButtonProps = React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     render?: React.ReactElement;
-  }) {
+  };
+
+function Button({ className, variant, size, render, children, ...props }: ButtonProps) {
   return useRender({
     defaultTagName: "button",
     render,
@@ -58,4 +53,4 @@ function Button({
   });
 }
 
-export { Button, buttonVariants };
+export { Button, buttonVariants, type ButtonProps };

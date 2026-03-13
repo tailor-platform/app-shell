39873f3b092145bb488ffcc3e0c5b9cb1249d146
IzumiSy <beetle-noise@gmx.com>
2026-03-13 23:08:26 +0900

    Export Table and Dialog

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -77,3 +77,5 @@ export { Badge, badgeVariants, type BadgeProps } from "./components/badge";
 export { DescriptionCard, type DescriptionCardProps } from "./components/description-card";
 export { Layout, type LayoutProps } from "./components/layout/Layout";
 export { Button, buttonVariants, type ButtonProps } from "./components/button";
+export { Table } from "./components/table";
+export { Dialog } from "./components/dialog";

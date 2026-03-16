683e2f100f12c26b74b66893e09321f270fb12c1
IzumiSy <beetle-noise@gmx.com>
2026-03-16 15:53:25 +0900

    Fix tooltip arrow

--- packages/core/src/components/tooltip.tsx
+++ packages/core/src/components/tooltip.tsx
@@ -13,7 +13,11 @@ function Provider({
   children: React.ReactNode;
 }) {
   return (
-    <BaseTooltip.Provider data-slot="tooltip-provider" delay={delayDuration} {...props}>
+    <BaseTooltip.Provider
+      data-slot="tooltip-provider"
+      delay={delayDuration}
+      {...props}
+    >
       {children}
     </BaseTooltip.Provider>
   );
@@ -53,7 +57,9 @@ function Root({ children, ...props }: TooltipRootProps) {
 Root.displayName = "Tooltip.Root";
 
 /** The element that triggers the tooltip on hover or focus. */
-function Trigger({ ...props }: React.ComponentProps<typeof BaseTooltip.Trigger>) {
+function Trigger({
+  ...props
+}: React.ComponentProps<typeof BaseTooltip.Trigger>) {
   return <BaseTooltip.Trigger data-slot="tooltip-trigger" {...props} />;
 }
 Trigger.displayName = "Tooltip.Trigger";
@@ -61,7 +67,7 @@ Trigger.displayName = "Tooltip.Trigger";
 /** The tooltip popup that displays additional information. */
 function Content({
   className,
-  sideOffset = 0,
+  sideOffset = 5,
   side = "top",
   align = "center",
   children,
@@ -77,13 +83,13 @@ function Content({
         <BaseTooltip.Popup
           data-slot="tooltip-content"
           className={cn(
-            "astw:bg-primary astw:text-primary-foreground astw:animate-in astw:fade-in-0 astw:zoom-in-95 astw:data-ending-style:animate-out astw:data-ending-style:fade-out-0 astw:data-ending-style:zoom-out-95 astw:data-[side=bottom]:slide-in-from-top-2 astw:data-[side=left]:slide-in-from-right-2 astw:data-[side=right]:slide-in-from-left-2 astw:data-[side=top]:slide-in-from-bottom-2 astw:z-50 astw:w-fit astw:origin-(--transform-origin) astw:rounded-md astw:px-3 astw:py-1.5 astw:text-xs astw:text-balance",
+            "astw:bg-primary astw:text-primary-foreground astw:animate-in astw:fade-in-0 astw:zoom-in-95 astw:data-ending-style:animate-out astw:data-ending-style:fade-out-0 astw:data-ending-style:zoom-out-95 astw:data-[side=bottom]:slide-in-from-top-2 astw:data-[side=left]:slide-in-from-right-2 astw:data-[side=right]:slide-in-from-left-2 astw:data-[side=top]:slide-in-from-bottom-2 astw:z-50 astw:w-fit astw:overflow-visible astw:origin-(--transform-origin) astw:rounded-md astw:px-3 astw:py-1.5 astw:text-xs astw:text-balance",
             className,
           )}
           {...restProps}
         >
           {children}
-          <BaseTooltip.Arrow className="astw:bg-primary astw:fill-primary astw:z-50 astw:size-2.5 astw:translate-y-[calc(-50%_-_2px)] astw:rotate-45 astw:rounded-[2px]" />
+          <BaseTooltip.Arrow className="astw:size-2.5 astw:rotate-45 astw:bg-primary astw:data-[side=top]:-bottom-1 astw:data-[side=bottom]:-top-1 astw:data-[side=left]:-right-1 astw:data-[side=right]:-left-1" />
         </BaseTooltip.Popup>
       </BaseTooltip.Positioner>
     </BaseTooltip.Portal>

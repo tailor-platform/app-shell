0771cd9b078eb8c158a4e6eca76e577ef957eb7f
IzumiSy <beetle-noise@gmx.com>
2026-03-23 14:22:02 +0900

    Add doc comments

--- packages/core/src/components/form.test.tsx
+++ packages/core/src/components/form.test.tsx
@@ -30,6 +30,17 @@ describe("Form", () => {
       );
       expect(container.innerHTML).toMatchSnapshot();
     });
+
+    it("form with noValidate", () => {
+      const { container } = render(
+        <Form noValidate>
+          <Field.Root name="email">
+            <Field.Control type="email" required />
+          </Field.Root>
+        </Form>,
+      );
+      expect(container.innerHTML).toMatchSnapshot();
+    });
   });
 
   it("renders children", () => {
@@ -48,5 +59,25 @@ describe("Form", () => {
       </Form>,
     );
     expect(container.querySelector("form")).toBeDefined();
+  });
+
+  it("sets noValidate on the form element", () => {
+    const { container } = render(
+      <Form noValidate>
+        <span>Content</span>
+      </Form>,
+    );
+    const form = container.querySelector("form");
+    expect(form?.noValidate).toBe(true);
+  });
+
+  it("forwards ref to the form element", () => {
+    const ref = { current: null } as React.RefObject<HTMLFormElement | null>;
+    render(
+      <Form ref={ref}>
+        <span>Content</span>
+      </Form>,
+    );
+    expect(ref.current).toBeInstanceOf(HTMLFormElement);
   });
 });
